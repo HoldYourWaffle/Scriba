1e43bcc834a5fa53b6514d7627dd006611e7faef
HoldYourWaffle <ravivanrooijen@live.nl>
2021-02-25 00:38:29 +0100

    Implement answers fetching

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ attempts.forEach(attemptRow => {
 	monk.style.cursor = 'pointer';
 
 	const magicButton = document.createElement('a');
-	//magicButton.onclick = NOW
+	magicButton.onclick = () => doMonkTask(cmid, attemptId);
 	magicButton.appendChild(monk);
 
 	const magicCell = document.createElement('td');
@@ -33,3 +33,69 @@ attempts.forEach(attemptRow => {
 	attemptRow.getElementsByClassName('$scriba_monk')[ 0 ]?.remove();
 	attemptRow.appendChild(magicCell);
 })
+
+
+// Would love to put this in a separate module, but browser extensions are not allowed to use ESM
+//  Using a build system just for this seems a bit much
+
+async function doMonkTask(cmid: number, attempt: number) {
+	const answers = await getAnswersForAttempt(cmid, attempt);
+	console.log(answers);
+
+	//NOW POST answers to ongoing attempt (/processattempt.php?cmid=XYZ)
+	
+	//SOON display some kind of message (the magical monk has done his magic)
+}
+
+
+async function getAnswersForAttempt(cmid: number, attempt: number): Promise<QuestionAnswer[]> {
+	//XXX does this have pagination?
+	const response = await fetch(`https://oncourse.tue.nl/2020/mod/quiz/review.php?attempt=${attempt}&cmid=${cmid}`);
+	const html = parser.parseFromString(await response.text(), 'text/html');
+
+	const questionAnswers: QuestionAnswer[] = [];
+
+	html.querySelectorAll('.que').forEach(question => {
+		const realIndex = parseInt(question.id.substring(question.id.lastIndexOf('-') + 1));
+
+		if (question.classList.contains('informationitem')) {
+			console.log(`Question element ${realIndex} in informational -> skipping`);
+			return;
+		}
+
+		if (!question.classList.contains('multichoice')) {
+			throw new Error(`Unknown question type: ${question.classList}`);
+		}
+
+
+		const answerInputs = question.querySelectorAll('.content .formulation .ablock .answer input') as NodeListOf<HTMLInputElement>;
+
+
+		const answer: { [ answerText: string ]: boolean } = {};
+
+		answerInputs.forEach(answerInput => {
+			if (answerInput.type !== 'checkbox' && answerInput.type !== 'radio') {
+				throw new Error(`Unknown answer input type: ${answerInput.type}`);
+			}
+			
+			const answerLabel = answerInput.labels![0];
+			let answerText = '';
+			for (let i = 0; i < answerLabel.childNodes.length; ++i) {
+				if (answerLabel.childNodes[i].nodeType === Node.TEXT_NODE) {
+					answerText += answerLabel.childNodes[i].textContent;
+				}
+			}
+
+			answer[answerText] = answerInput.hasAttribute('checked');
+		});
+
+
+		questionAnswers.push({
+			realIndex, answer,
+			visibleIndex: questionAnswers.length + 1,
+			correct: question.classList.contains('correct')
+		})
+	})
+
+	return questionAnswers;
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -3,6 +3,24 @@ import { Browser } from 'webextension-polyfill-ts'
 declare global {
 	// Ambient module declaration to expose modular definitions as global
 	const browser: Browser;
+	
+	// Declaring in global scope because modules would require a build setup
+
+	interface QuestionAnswer {
+		/** Index in the DOM */
+		realIndex: number;
+		
+		/** Question number visible to user */
+		visibleIndex: number;
+		
+		/** XXX for now we're only dealing with simple yes/no questions */
+		answer: { [answerText: string]: boolean };
+		
+		//TODO partial correctness, i.e. some answers of a checkbox were correct
+		correct: boolean;
+		
+		//TODO flagedness
+	}
 }
 
 
